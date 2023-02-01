4f93dd64223a40519503e30906cad14b19854f50
Alex MacCalman <alex.maccalman@gmail.com>
2023-01-31 21:42:00 -0500

    blocked my ip

--- back_end/api/GetResumeCounter/__init__.py
+++ back_end/api/GetResumeCounter/__init__.py
@@ -2,14 +2,18 @@ from email.headerregistry import ContentTypeHeader
 from importlib.util import resolve_name
 import json
 import logging
+import socket
 
 import azure.functions as func
+DISALLOWED_IP = '73.212.171.225'
 
 def main(req: func.HttpRequest, readdb: func.DocumentList, updatedb: func.Out[func.Document]) -> func.HttpResponse:
-    test = "dummy2"
+    client_ip = req.headers.get("X-Forwarded-For") or req.headers.get("X-Real-IP") or socket.gethostbyname(socket.gethostname())
     currentrow = readdb[0].data
-    #print(type(readdb[0].data))
-    currentrow['count'] = currentrow['count'] + 1
+    
+    if client_ip != DISALLOWED_IP:
+        currentrow['count'] += 1
+        updatedb.set(func.Document.from_dict(currentrow))
     
     # write to the cosmos DB the new count
     response = func.HttpResponse(
